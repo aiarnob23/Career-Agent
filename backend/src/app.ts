0d265b1418b4e841f0d94995e244823821b35591
aiarnob23 <aiarnob23@gmail.com>
2025-06-20 20:33:07 +0600

    cors updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://career-agent-murex.vercel.app",
+    origin: "https://career-agent-frontend.vercel.app",
     credentials: true,
   })
 );
